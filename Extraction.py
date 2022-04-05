c9d9fc3f3e9f5d07c1d703969cd986a78679c5d5
AstrixDj13 <80380546+AstrixDj13@users.noreply.github.com>
2022-04-05 16:38:55 +0200

    Update Extraction.py with comments

--- Extraction.py
+++ Extraction.py
@@ -8,17 +8,17 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
-file = pd.read_csv("output_street.csv")
-data = np.array(file['shapeid'].unique())
+file = pd.read_csv("output_street.csv")     #Reading the csv file containing street nodes
+data = np.array(file['shapeid'].unique())   #storing the unique shape-ids in an array
 #print(data)
 grid_list = (np.array_split(data,size))[rank]
 
 #print(file.X_Coordinate)
 
-nodalmatrix = np.zeros((2,2))  # A matrix that stores the coordinates of the two points
-your_mesh = mesh.Mesh.from_file('Region_of_Interest.stl')
+nodalmatrix = np.zeros((2,2))  # A matrix that stores the coordinates of the two nodes of a street
+your_mesh = mesh.Mesh.from_file('Region_of_Interest.stl')   #STL file from which streets are to be extracted
 
-cogs = np.zeros((your_mesh.data.size, 5)) #This section converts every triangle in a point (Center of gravity)
+cogs = np.zeros((your_mesh.data.size, 5)) #This section converts every triangle to a point (Center of gravity)
 for i in range(0, your_mesh.data.size): #i refers to the rows of the array cog
     for j in range(0, 4):  # i refers to the columns of the array cog
         if j == 0:  #j=1 always refers to the calculation of the x cog coordinate for the triangle i
@@ -32,7 +32,7 @@ for i in range(0, your_mesh.data.size): #i refers to the rows of the array cog
 #print(cogs)
 k = 0
 streettriangle =[]
-for i in range(2125):
+for i in range(2125):     #loop which runs through all the the points in the csv file
 
     if file.shapeid[i] == grid_list[k] and file.shapeid[i+1] == grid_list[k]:
         '''for i in range(2):  # A for loop for the number of nodes
@@ -41,17 +41,17 @@ for i in range(2125):
             for j in range(2):  # A for loop for the coordinates of each node
                 coordinate.append(int(input()))
             nodalmatrix.append(coordinate)'''
-        nodalmatrix[0,0] = file.X_Coordinate[i]
-        nodalmatrix[0,1] = file.Y_Coordinate[i]
-        nodalmatrix[1,0] = file.X_Coordinate[i+1]
-        nodalmatrix[1,1] = file.Y_Coordinate[i+1]
+        nodalmatrix[0,0] = file.X_Coordinate[i]     #Stores the X and Y cooedinates of the
+        nodalmatrix[0,1] = file.Y_Coordinate[i]     #two nodes
+        nodalmatrix[1,0] = file.X_Coordinate[i+1]   #in the nodalmatrix
+        nodalmatrix[1,1] = file.Y_Coordinate[i+1]   #numpy array
 
         delta_x = nodalmatrix[0,0] - nodalmatrix[1,0]
         delta_y = nodalmatrix[0,1] - nodalmatrix[1,1]
         theta = math.atan(delta_y/delta_x)
-        x1 = nodalmatrix[0,0] + (5 * math.sin(theta))
-        y1 = nodalmatrix[0,1] - (5 * math.cos(theta))
-        x2 = nodalmatrix[0,0] - (5 * math.sin(theta))
+        x1 = nodalmatrix[0,0] + (5 * math.sin(theta))   #Calculates the 4 corner coordinates of
+        y1 = nodalmatrix[0,1] - (5 * math.cos(theta))   #rectangle formed by taking a buffer
+        x2 = nodalmatrix[0,0] - (5 * math.sin(theta))   #of 5 on both sides 
         y2 = nodalmatrix[0,1] + (5 * math.cos(theta))
         x3 = nodalmatrix[1,0] + (5 * math.sin(theta))
         y3 = nodalmatrix[1,1] - (5 * math.cos(theta))
@@ -68,10 +68,10 @@ for i in range(2125):
         '''print("ID of the triangles inside this street:  ")
         counter=0'''
         
-        for j in range(0, your_mesh.data.size):
-            trianglecoordinate[0,0] = cogs[j,0]
-            trianglecoordinate[0,1] = cogs[j,1]
-
+        for j in range(0, your_mesh.data.size):     #loop which runs through all the triangles in the mesh
+            trianglecoordinate[0,0] = cogs[j,0]     #stores the coordinates of the centroid
+            trianglecoordinate[0,1] = cogs[j,1]     #of each triangle
+            #Logic to check whether the cebtroid of each triangle is inside the rectangle formed above
             Mat1 = np.array([[x1, y1, 1], [x2, y2, 1], [trianglecoordinate[0,0], trianglecoordinate[0,1], 1]])
             Mat2 = np.array([[x1, y1, 1], [x3, y3, 1], [trianglecoordinate[0, 0], trianglecoordinate[0, 1], 1]])
             Mat3 = np.array([[x3, y3, 1], [x4, y4, 1], [trianglecoordinate[0, 0], trianglecoordinate[0, 1], 1]])
@@ -83,14 +83,14 @@ for i in range(2125):
             A = 10 * math.sqrt(math.pow(delta_x, 2) + math.pow(delta_y, 2))
             Atot = A1 + A2 + A3 + A4
 
-
+#If the triangle is inside then stores the coordinates of the vertices of the triangle in street variable and appends all the triangles one by one to streettriangle matrix
             if Atot > A*0.999999 and Atot < A*1.000001:
-                street =np.array([j,your_mesh.x[j,0],your_mesh.y[j,0],your_mesh.x[j,1],your_mesh.y[j,1],your_mesh.x[j,2],your_mesh.y[j,2]])
+                street =np.array([j,your_mesh.x[j,0],your_mesh.y[j,0],your_mesh.x[j,1],your_mesh.y[j,1],your_mesh.x[j,2],your_mesh.y[j,2]])     
                 streettriangle.append(street)
 
     elif file.shapeid[i] < grid_list[k]:
         continue
-
+#Generates all stl streets and empties the streettriangle matrix for the next iteration (shape-id)
     else:
         rows = len(streettriangle)
         generate = np.zeros(rows, dtype=mesh.Mesh.dtype)
